1ad9a03c0598196879ea8977fb466d267eab65a3
glen1314 <1621041508@qq.com>
2025-12-07 15:26:27 +0800

    C语言更新20251207

--- testC.c
+++ testC.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*
+ *整型：基本整型（int）、短整型（short int），长整型（long int）
+ *浮点型：单精度型（float）、双精度型（double）
+ *字符型（char）
+ *指针类型
+ *构造类型：数组（[]）、结构体（struct）
+ *空类型（void）
+ *
+ */
 
 int main(void) {
     
@@ -9,10 +18,10 @@ int main(void) {
     // printf("So you have %d dog(s)!\n",dogs);
 
     //定义多个变量
-    int a,b,c;
-    float m = 0.27,n=93.25;
-    char p,q = '#';
-    printf("默认格式: %f\n", m);
+    // int a,b,c;
+    // float m = 0.27,n=93.25;
+    // char p,q = '#';
+    // printf("默认格式: %f\n", m);
 
 
 
@@ -22,6 +31,25 @@ int main(void) {
     printf("I am a simple");
     printf("computer.\n");
     printf("My favorite number is %d because it is first.\n",num);
+
+    printf("start=========================整型=========================start\n");
+    short i = 10;
+    printf("%d\n",i);
+
+    int a;
+    short int b;
+    long int c;
+
+    float d;
+    double e;
+
+    char f;
+
+    printf("int = %d\n",sizeof(a));
+
+    printf("stop=========================整型=========================stop\n");
     return 0;
+
+
 
 }
